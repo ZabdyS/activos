6fa6a618cbf36cf8c12eae0ebf7c2f08369e65b7
ZabdyS <78625284+ZabdyS@users.noreply.github.com>
2024-03-08 19:42:48 -0400

    NoSe tengo miedo bro

---
+++ Models/TipoActivo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace activos.Models
+{
+    public class TipoActivo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_tipo_activo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Descripcion { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? CuentaContableCompra { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? CuentaContableDepreciacion { get; set; }
+
+        public bool Estado { get; set; }
+
+        // Propiedad de navegación para activos fijos
+        public ICollection<ActivoFijo>? ActivosFijos { get; set; }
+    }
+}
