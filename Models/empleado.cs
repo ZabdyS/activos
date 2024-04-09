b5e3e1a8ad3e60d73d66280a77a3a5f629f21bf5
Mei Yu Tan Deng <m.tan1@unapec.edu.do>
2024-04-09 03:35:23 -0400

    Validacion de cedula

--- Models/empleado.cs
+++ Models/empleado.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 
 namespace activos.Models
 {
@@ -16,6 +17,7 @@ namespace activos.Models
 
         [Required]
         [StringLength(20)]
+        [CustomValidation(typeof(Utilidades), nameof(Utilidades.ValidaCedula))]
         public string Cedula { get; set; }
 
         // Propiedad de navegación para el departamento
@@ -32,5 +34,49 @@ namespace activos.Models
         public DateTime FechaIngreso { get; set; }
 
         public bool Estado { get; set; }
+    }
+
+    public static class Utilidades
+    {
+        public static ValidationResult ValidaCedula(string pCedula)
+        {
+            if (!EsNumerico(pCedula))
+            {
+                return new ValidationResult("La cédula solo puede contener caracteres numéricos.", new[] { "Cedula" });
+            }
+            int vnTotal = 0;
+            string vcCedula = pCedula.Replace("-", "");
+            int pLongCed = vcCedula.Trim().Length;
+            int[] digitoMult = new int[11] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+
+            if (pLongCed != 11)
+                return new ValidationResult("La cédula debe tener 11 caracteres.", new[] { "Cedula" });
+
+            for (int vDig = 1; vDig <= pLongCed; vDig++)
+            {
+                int vCalculo = int.Parse(vcCedula.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                if (vCalculo < 10)
+                    vnTotal += vCalculo;
+                else
+                    vnTotal += int.Parse(vCalculo.ToString().Substring(0, 1)) + int.Parse(vCalculo.ToString().Substring(1, 1));
+            }
+
+            if (vnTotal % 10 == 0)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult("La cédula no es válida.", new[] { "Cedula" });
+
+        }
+        private static bool EsNumerico(string valor)
+        {
+            foreach (char c in valor)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
