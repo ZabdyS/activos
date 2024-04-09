e9e982e685a8011223ad2b32afafa4e3412884eb
ALEJANDRO ROSARIO D OLEO <a.rosariod@unapec.edu.do>
2024-04-09 03:48:14 -0400

    SI FUNCIONAAAAAA

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using activos.Models;
 using Microsoft.EntityFrameworkCore;
+using activos.Service;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 
 
 
@@ -10,6 +13,14 @@ builder.Services.AddControllersWithViews();
 //conexion
 builder.Services.AddDbContext<MyDbContext>(options =>
 options.UseMySql("name=ConnectionStrings:DefaultConnection", new MySqlServerVersion(new Version(8, 0, 26))));
+
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.LoginPath = "/Inicio/IniciarSesion";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+});
 
 var app = builder.Build();
 
@@ -27,11 +38,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Inicio}/{action=IniciarSesion}/{id?}");
 
 app.Run();
---
+++ Service/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using activos.Models;
+
+namespace activos.Service
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> GetUsuario(string email, string clave);
+        Task<Usuario> SaveUsuario(Usuario usuario);
+    }
+}
